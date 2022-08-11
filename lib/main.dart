c77006c7570d5a9c87077b8a49fb2aa0057e8b78
Vlarimit N. Yatsuk <yatsukvn@gmail.com>
2022-08-11 15:39:05 +0600

    ~ fix

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: CounterWidget(),
+      home: const CounterWidget(),
     );
   }
 }
@@ -35,89 +35,87 @@ class _CounterWidgetState extends State {
         centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Tap to "-" to decrement',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Tap to "-" to decrement',
+              style: TextStyle(
+                color: Colors.white,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Ink(
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Ink(
+                  decoration: const ShapeDecoration(
+                    color: Colors.white60,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        topRight: Radius.zero,
+                        bottomLeft: Radius.circular(8),
+                        bottomRight: Radius.zero,
+                      ),
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _count -= 1;
+                      });
+                    },
+                    icon: const Icon(Icons.remove),
+                    color: Colors.black,
+                  ),
+                ),
+                Ink(
                     decoration: const ShapeDecoration(
                       color: Colors.white60,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          topRight: Radius.zero,
-                          bottomLeft: Radius.circular(8),
-                          bottomRight: Radius.zero,
-                        ),
+                        borderRadius: BorderRadius.zero,
                       ),
                     ),
-                    child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _count -= 1;
-                        });
-                      },
-                      icon: const Icon(Icons.remove),
-                      color: Colors.black,
+                    child: Padding(
+                      padding: const EdgeInsets.all(9.5),
+                      child: Text(
+                        '$_count',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    )),
+                Ink(
+                  decoration: const ShapeDecoration(
+                    color: Colors.white60,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.zero,
+                        topRight: Radius.circular(8),
+                        bottomLeft: Radius.zero,
+                        bottomRight: Radius.circular(8),
+                      ),
                     ),
                   ),
-                  Ink(
-                      decoration: const ShapeDecoration(
-                        color: Colors.white60,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(9.5),
-                        child: Text(
-                          '$_count',
-                          style: const TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                      )),
-                  Ink(
-                    decoration: const ShapeDecoration(
-                      color: Colors.white60,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.zero,
-                          topRight: Radius.circular(8),
-                          bottomLeft: Radius.zero,
-                          bottomRight: Radius.circular(8),
-                        ),
-                      ),
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _count += 1;
-                        });
-                      },
-                      icon: const Icon(Icons.add),
-                      color: Colors.black,
-                    ),
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _count += 1;
+                      });
+                    },
+                    icon: const Icon(Icons.add),
+                    color: Colors.black,
                   ),
-                ],
-              ),
-              const Text(
-                'Tap to "+" to increment',
-                style: TextStyle(
-                  color: Colors.white,
                 ),
-              )
-            ],
-          ),
+              ],
+            ),
+            const Text(
+              'Tap to "+" to increment',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
         ),
       ),
     );
